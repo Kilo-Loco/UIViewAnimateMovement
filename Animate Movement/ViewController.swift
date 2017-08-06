9a3cdb38a90c7aa9ea5b7c7e82357d668da3d119
Kyle <kiloloco@me.com>
2017-08-06 12:43:30 -0700

    Starter project

---
+++ Animate Movement/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Animate Movement
+//
+//  Created by Kyle Lee on 8/6/17.
+//  Copyright © 2017 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var black1: UIView!
+    @IBOutlet weak var black2: UIView!
+    @IBOutlet weak var black3: UIView!
+    @IBOutlet weak var black4: UIView!
+    
+    @IBOutlet weak var red: UIView!
+    @IBOutlet weak var blue: UIView!
+    @IBOutlet weak var green: UIView!
+    @IBOutlet weak var orange: UIView!
+    @IBOutlet weak var purple: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onAnimateBlackTapped() {
+        
+    }
+    
+    @IBAction func onAnimateColorTapped() {
+        
+    }
+
+
+}
+
