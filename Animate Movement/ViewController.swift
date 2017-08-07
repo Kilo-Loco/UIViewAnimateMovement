ae156337b3feba22e0d827e823707ffa9903913b
Kyle <kiloloco@me.com>
2017-08-07 08:14:17 -0700

    Finished tutorial

--- Animate Movement/ViewController.swift
+++ Animate Movement/ViewController.swift
@@ -21,24 +21,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var orange: UIView!
     @IBOutlet weak var purple: UIView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func moveRight(view: UIView) {
+        view.center.x += 300
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func moveLeft(view: UIView) {
+        view.center.x -= 300
     }
     
     @IBAction func onAnimateBlackTapped() {
+        let duration: Double = 2.0
+        UIView.animate(withDuration: duration) {
+            self.moveRight(view: self.black1)
+        }
         
+        UIView.animate(withDuration: duration, animations: {
+            self.moveRight(view: self.black2)
+        }) { (finished) in
+            if finished {
+                UIView.animate(withDuration: duration, animations: {
+                    self.moveLeft(view: self.black2)
+                })
+            }
+        }
+        
+        let delay: Double = 1.0
+        UIView.animate(withDuration: duration, delay: delay, options: .curveLinear, animations: {
+            self.moveRight(view: self.black3)
+        }) { (_) in
+            self.moveLeft(view: self.black3)
+        }
+        
+        UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: 0.5, initialSpringVelocity: 2, options: .curveLinear, animations: {
+            self.moveRight(view: self.black4)
+        }) { (_) in
+//            self.moveLeft(view: self.black4)
+        }
     }
     
     @IBAction func onAnimateColorTapped() {
+        let duration = 2.0
+        UIView.animate(withDuration: duration, delay: 0, options: .curveLinear, animations: {
+            self.moveRight(view: self.red)
+        }) { (_) in
+            self.moveLeft(view: self.red)
+        }
         
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseIn, animations: {
+            self.moveRight(view: self.blue)
+        }) { (_) in
+            self.moveLeft(view: self.blue)
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: {
+            self.moveRight(view: self.green)
+        }) { (_) in
+            self.moveLeft(view: self.green)
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0, options: .autoreverse, animations: {
+            self.moveRight(view: self.orange)
+        }) { (_) in
+            self.moveLeft(view: self.orange)
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0, options: [.repeat, .autoreverse, .curveEaseIn] , animations: {
+            self.moveRight(view: self.purple)
+        }) { (_) in
+//            self.moveLeft(view: self.purple)
+        }
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
